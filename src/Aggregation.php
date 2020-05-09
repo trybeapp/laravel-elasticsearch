65075c079eaec74c896db958b9c6ec3002a39565
Will Taylor-Jackson <will.taylorjackson@gmail.com>
2020-05-09 21:38:29 +0100

    refactor: more intuitive and flexible class
    Non backwards-compatible changes

--- src/Aggregation.php
+++ src/Aggregation.php
@@ -2,29 +2,128 @@
 
 namespace DesignMyNight\Elasticsearch;
 
+use DesignMyNight\Elasticsearch\QueryBuilder;
+use Illuminate\Support\Str;
+
+/**
+ * `Aggregation` classes may be passed to the query builder's `aggregation()` method. Extend this class and include
+ * either class properties or methods to return the required aggregation key (string), type (string), arguments (as a
+ * string, array or QueryBuilder) and optional sub aggregations (as a QueryBuilder).
+ *
+ * The above nested aggregation could be carried out with `Aggregation` classes like so:
+ * ```
+ * $results = Customer::search()
+ *     ->where('dob_month', 11)
+ *     ->aggregation((new ByCustomerAggregation)
+ *         ->setSubAggregation(new TotalRevenueAggregation)
+ *     );
+ * ```
+ *
+ * If multiple sub aggregations are required, pass a `callable` to `setSubAggregation` which will be passed a
+ * `QueryBuilder` instance and apply the aggregations to that class, for example:
+ * ```
+ * $results = Customer::search()
+ *     ->aggregation((new ByCustomerAggregation)
+ *         ->setSubAggregation(function ($builder) {
+ *             $builder->aggregation(new TotalRevenueAggregation);
+ *             $builder->aggregation('total_transactions', 'count', 'revenue');
+ *         })
+ *     );
+ * ```
+ */
 abstract class Aggregation
 {
-    protected $arguments = [];
-    protected $key = '';
+    /**
+     * @var string
+     */
     protected $type = '';
 
-    public function __invoke(QueryBuilder $query): QueryBuilder
+    /**
+     * @var string
+     */
+    protected $key;
+
+    /**
+     * @var Aggregation
+     */
+    protected $subAggregation;
+
+    /**
+     * @param string $key
+     * @return self
+     */
+    public function setKey(string $key): self
     {
-        return $query;
+        $this->key = $key;
+
+        return $this;
     }
 
-    public function getArguments()
-    {
-        return $this->arguments;
-    }
-
-    public function getKey(): string
-    {
-        return $this->key;
-    }
-
+    /**
+     * @return string
+     */
     public function getType(): string
     {
         return $this->type;
+    }
+
+    /**
+     * @return string
+     */
+    public function getKey(): string
+    {
+        return $this->key ?: Str::snake(class_basename($this));
+    }
+
+    /**
+     * @param QueryBuilder $builder
+     * @return mixed
+     */
+    public function getArguments(QueryBuilder $builder)
+    {
+        return $this->arguments ?? [];
+    }
+
+    /**
+     * @return self
+     */
+    public function setSubAggregation($subAggregation): self
+    {
+        $this->subAggregation = $subAggregation;
+
+        return $this;
+    }
+
+    /**
+     * @param QueryBuilder $builder
+     * @return QueryBuilder|null
+     */
+    public function applySubAggregations(QueryBuilder $builder): ?QueryBuilder
+    {
+        if (isset($this->subAggregation)) {
+            if ($this->subAggregation instanceof Aggregation) {
+                $this->subAggregation->apply($builder);
+            } elseif (is_callable($this->subAggregation)) {
+                call_user_func($this->subAggregation, $builder);
+            }
+
+            return $builder;
+        }
+
+        return null;
+    }
+
+    /**
+     * @param QueryBuilder $builder
+     * @return QueryBuilder
+     */
+    public function apply(QueryBuilder $builder): QueryBuilder
+    {
+        return $builder->aggregation(
+            $this->getKey(),
+            $this->getType(),
+            $this->getArguments($builder->newQuery()),
+            $this->applySubAggregation($builder->newQuery())
+        );
     }
 }
