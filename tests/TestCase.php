be70e97440590ab608602d1a9955cebdf19b1897
Will Taylor-Jackson <will@try.be>
2022-05-23 11:41:12 +0100

    chore: remove unused

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,28 +2,10 @@
 
 namespace Tests;
 
-use Carbon\Carbon;
 use Orchestra\Testbench\TestCase as BaseTestCase;
 
 class TestCase extends BaseTestCase
 {
-
-    /**
-     * Set up the tests.
-     */
-    public function setUp()
-    {
-        parent::setUp();
-    }
-
-    /**
-     * @return void
-     */
-    public function tearDown()
-    {
-        parent::tearDown();
-    }
-
     /**
      * @param \Illuminate\Foundation\Application $app
      */
